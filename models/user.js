52cb7e522938e7acc86b73acdd1c41a94c21623f
Gvozdenko Sergey <exetto@gmail.com>
2023-08-17 13:02:09 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -1,68 +1,68 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const UnautorizedError = require("../errors/UnautorizedError");
-const urlRegex = require("../utils/constants");
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const UnautorizedError = require('../errors/UnautorizedError');
+const urlRegex = require('../utils/constants');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      default: "Жак-Ив Кусто",
-      minlength: [2, "Минимальная длинна 2"],
-      maxlength: [30, "Максимальная длинна 30"],
+      default: 'Жак-Ив Кусто',
+      minlength: [2, 'Минимальная длинна 2'],
+      maxlength: [30, 'Максимальная длинна 30'],
     },
     about: {
       type: String,
-      default: "Исследователь",
-      minlength: [2, "Минимальная длинна 2"],
-      maxlength: [30, "Максимальная длинна 30"],
+      default: 'Исследователь',
+      minlength: [2, 'Минимальная длинна 2'],
+      maxlength: [30, 'Максимальная длинна 30'],
     },
     avatar: {
       type: String,
       default:
-        "https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png",
+        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
       validate: {
         validator(url) {
           return urlRegex.test(url);
         },
-        message: "Введите URL",
+        message: 'Введите URL',
       },
     },
     email: {
       type: String,
-      required: [true, "Поле должно быть заполнено"],
+      required: [true, 'Поле должно быть заполнено'],
       unique: true,
       validate: {
         validator(email) {
           validator.isEmail(email);
         },
-        message: "Введите верный email",
+        message: 'Введите верный email',
       },
     },
     password: {
       type: String,
-      required: [true, "Поле должно быть заполнено"],
+      required: [true, 'Поле должно быть заполнено'],
       select: false,
     },
   },
-  { versionKey: false }
+  { versionKey: false },
 );
 
 userSchema.statics.findUserByCredentials = function findUserByCredentials(
   email,
-  password
+  password,
 ) {
   return this.findOne({ email })
-    .select("+password")
+    .select('+password')
     .then((user) => {
       if (!user) {
-        throw new UnautorizedError("Неправильные почта или пароль");
+        throw new UnautorizedError('Неправильные почта или пароль');
       }
 
       return bcrypt.compare(password, user.password).then((matched) => {
         if (!matched) {
-          throw new UnautorizedError("Неправильные почта или пароль");
+          throw new UnautorizedError('Неправильные почта или пароль');
         }
 
         return user;
@@ -70,4 +70,4 @@ userSchema.statics.findUserByCredentials = function findUserByCredentials(
     });
 };
 
-module.exports = mongoose.model("user", userSchema);
+module.exports = mongoose.model('user', userSchema);
